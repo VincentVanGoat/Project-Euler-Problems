e72d406bef3d947b916106e116f524e0c48ff5ed
vincentvangoat <menogetusername@gmail.com>
2015-11-03 14:58:04 -0500

    Solved problem05

---
+++ problem05.py
@@ -0,0 +1,8 @@
+def main():
+	i = 2520
+	while not(i%11 == 0 and i%12 == 0 and i%13 == 0 and i%14 == 0 and i%15 == 0 and i%16 == 0 and i%17 == 0 and i%18 == 0 and i%19 == 0 and i%20 == 0):
+		i+=1
+	return i 
+
+if __name__ == "__main__":
+	print main()
